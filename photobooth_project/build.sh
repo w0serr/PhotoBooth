0ba87a83889e1134033b60b944bd499b6cbe66b6
w0serr <vzn.sergey@gmail.com>
2025-04-17 22:43:25 +0900

    Delete photobooth_project/build.sh

--- photobooth_project/build.sh
+++
@@ -1,6 +0,0 @@
-# Install dependencies with --no-cache-dir to reduce size
-pip3 install --no-cache-dir -r requirements.txt
-
-# Collect static files and apply migrations
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
